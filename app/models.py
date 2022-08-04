998eb124b4a772f37dcd82a7c6795dafff58a57a
Maggie Chege <shikooh.maggie@gmail.com>
2022-08-04 14:25:39 +0300

    - Change DB config to user Postgres - Ensure Alambic handles migrations(Not completely tested - Add tasks to handle creating and closing of DB Connections - Capture important secrets in config from .env file

--- app/models.py
+++ app/models.py
@@ -1,7 +1,22 @@
 from pydantic import BaseModel
 from typing import Optional
+from core.db import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
-class User(BaseModel):
-    is_active: bool
-    bio: Optional[str]
-    email: str
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    bio = Column(String)
+    is_active = Column(Boolean, default=True)
+    email = Column(String, unique=True, index=True)
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
